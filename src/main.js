77bc38bac31b3d6f927a741caa298aabe2d3ebd3
Potato0119 <502ender502@gmail.com>
2023-08-03 13:09:25 +0800

    專案初始化

---
+++ src/main.js
@@ -0,0 +1,19 @@
+// Require檔案(discord.js, dotenv)
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+import dotenv from "dotenv"
+
+dotenv.config()
+
+// 創建此機器人 client
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// 註冊一次性事件 (上線)
+client.once(Events.ClientReady, c => {
+	console.log(`${c.user.username}! 啟動!`);
+});
+
+
+
+
+// token Input
+client.login(process.env.TOKEN);
